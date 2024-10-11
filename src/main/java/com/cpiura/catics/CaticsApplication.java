cb2518f6b14adef0a8761079a5511f8ee4c2db17
lumod97 <luiggigmd_97@gmail.com>
2024-10-11 01:38:49 -0500

    first upload, US01 AND US02 finished

---
+++ src/main/java/com/cpiura/catics/CaticsApplication.java
@@ -0,0 +1,13 @@
+package com.cpiura.catics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpiura/catics/CaticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpiura.catics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
