343ce951803a11584ab9f3546351518e1b44cc8e
Luiggi Moretti Dioses <luiggigmd_97@outlook.com>
2024-10-21 08:23:32 -0500

    ticket controller liberado

--- src/main/java/com/cpiura/catics/controller/TicketController.java
+++ src/main/java/com/cpiura/catics/controller/TicketController.java
@@ -1,25 +1,25 @@
-// package com.cpiura.catics.controller;
+package com.cpiura.catics.controller;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// import com.cpiura.catics.entity.Ticket;
-// import com.cpiura.catics.service.TicketService;
+import com.cpiura.catics.entity.Ticket;
+import com.cpiura.catics.service.TicketService;
 
 
 
-// @RestController
-// public class TicketController {
+@RestController
+public class TicketController {
 
-//     @Autowired
-//     private TicketService ticketService;
+    @Autowired
+    private TicketService ticketService;
 
-//     @GetMapping("/tickets/top10")
-//     public List<Ticket> getTop10Tickets() {
-//         return ticketService.getTop10Tickets(); // Calls the service to get the tickets
-//     }
+    @GetMapping("/tickets/top10")
+    public List<Ticket> getTop10Tickets() {
+        return ticketService.getTop10Tickets(); // Calls the service to get the tickets
+    }
     
-// }
+}
